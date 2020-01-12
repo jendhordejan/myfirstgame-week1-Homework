313beacf1022f5efa7190a74f4df2a2152dad9d8
jendhordejan <jendhordejan@gmail.com>
2020-01-12 09:54:40 +0100

    created hero{}

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,8 @@
 // Write your JS here
+// create an object hero{}
+const hero = {name: 'XStriKeR',
+              heroic: true,
+              inventory: [],
+              health: 10,
+              weapon: {type: 'wooden sword', damage: 2}}
+              ;
