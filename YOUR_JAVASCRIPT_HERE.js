0e3b5bafcc04865de6726c6426c7b14bfb34c9b4
jendhordejan <jendhordejan@gmail.com>
2020-01-12 21:35:02 +0100

    add battle functions

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -12,7 +12,8 @@ const monst1 = {name: 'goblin',
                 health: 5,
                 weapon: {type: 'club', damage: 1},
                 itemDrop: {type: 'club', damage: 1},
-                winChant: 'Gobel! Gobel!'
+                winChant: 'Gobel! Gobel!',
+                imgSrc: 'images/imp.jpeg'
 };
 
 const monst2 = {name: 'imp',
@@ -20,22 +21,48 @@ const monst2 = {name: 'imp',
                 health: 5,
                 weapon: {type: 'claw', damage: 2},
                 itemDrop: {type: 'claw', damage: 2},
-                winChant: 'EEEEEEK! EEEK!'
+                winChant: 'EEEEEEK! EEEK!',
+                imgSrc: 'images/imp.jpeg'
 };
 
-const monst3 = {name: 'Reijn',
+const monst3 = {name: 'Ancient Codeisseur Instructor (Reijn)',
                 heroic: false,
                 health: 500,
-                weapon: {type: 'claw', damage: 2},
-                itemDrop: {type: 'claw', damage: 2},
-                winChant: 'You are not Strong and Independent and skillful enough to beat me! bwahaha'
+                weapon: {type: 'Sword of Revealing Bug', damage: 500},
+                itemDrop: {type: 'Enchanted Codes', damage: 500},
+                winChant: 'You are not Strong and Independent and skillful enough to beat me! bwahaha',
+                imgSrc: 'images/Reijn.jpeg'
 };
 
-const monsterList = [monst1, monst2, monst3];
+const monst4 = {name: 'Ancient Codeisseur Instructor (Kelly)',
+                heroic: false,
+                health: 500,
+                weapon: {type: 'Magestic Recipe Book', damage: 500},
+                itemDrop: {type: 'Enchanted Codes', damage: 500},
+                winChant: 'OMG! What am I doing?... whatever...',
+                imgSrc: 'images/Kelly.jpeg'
+};
+
+const monst5 = {name: 'Ancient Codeisseur Instructor (David)',
+                heroic: false,
+                health: 500,
+                weapon: {type: 'Claws of Games', damage: 500},
+                itemDrop: {type: 'Enchanted Codes', damage: 500},
+                winChant: 'TIK-TIK-TIK...You are no match for my speed!',
+                imgSrc: 'images/David.jpeg'
+};
+
+const monsterList = [monst1, monst2, monst3, monst4, monst5];
 let selectedMonster ={};
 
 let skipInventoryLoad = false;
 let allowChangeHeroName = true;
+var divBattle = document.getElementById("battleArea");
+
+function setGameInit(){
+  divBattle.style.display = "none"
+  displaystats(hero);
+}
 
 function displaystats(person){
   //display name : hero.name
@@ -65,8 +92,23 @@ function displaystats(person){
     }
 }
 
-displaystats(hero);
+function displayMonsterStats(monster){
+  const monsterImgEle = document.getElementById("monsterImage")
+  monsterImgEle.setAttribute("src", monster.imgSrc);
+  
+  //display name : hero.name
+  const monsterNameEle = document.getElementById("monsterName");
+  monsterNameEle.innerHTML= `Name  :   ${monster.name}`;
 
+  //display health : hero.health
+  const monsterHealthEle = document.getElementById("monsterHealth")
+  monsterHealthEle.innerHTML= `Health :    ${monster.health}`
+
+  //display name : hero.name
+  const monsterWeapEle = document.getElementById("monsterWeapon");
+  monsterWeapEle.innerHTML= `Weapon  :   ${monster.weapon.type} - ${monster.weapon.damage}`;
+
+}
 
 // game logic functions
 function rest(person){
@@ -94,14 +136,60 @@ function equipWeapon(){
   displaystats(hero)
 }
 
-
-
 function getRandomMonster(monsters){
   // let mon = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men', 
   // 'Breaking Bad', 'Modern Family', 'Game of Thrones', 'Dexter'];
   selectedMonster = monsters[Math.floor(Math.random() * monsters.length)];
   return selectedMonster;
 }
+
+function initBattle() {
+  
+  if (divBattle.style.display === "none") {
+    divBattle.style.display = "block";
+  } else {
+    divBattle.style.display = "none";
+  }
+}
+
+setGameInit();
+
+function attack(){
+  let atkDmg = hero.weapon.damage;
+  selectedMonster.health=selectedMonster.health-atkDmg;
+  if (selectedMonster.health>0){
+    window.alert(`${selectedMonster.name} takes -${atkDmg}`);
+    displayMonsterStats(selectedMonster);
+
+    monsterAttack();
+  } else {
+    window.alert('You WIN! Great Job!');
+    divBattle.style.display = "none"
+    return;
+  }
+}
+
+function monsterAttack(){
+  let atkDmg = selectedMonster.weapon.damage;
+  hero.health=hero.health-atkDmg;
+  if (hero.health>0){
+    window.alert(`you take -${atkDmg} from ${selectedMonster.name}`);
+    displaystats(hero);
+  } else {
+    window.alert(`${selectedMonster.name} says: ${selectedMonster.winChant}...YOU LOOSE! Try Again.`)
+    hero.health = 0;
+    displaystats(hero);
+    divBattle.style.display = "none"
+    return;
+  }
+}
+
+
+const isAtkBtnClicked = document.getElementById("attack");
+isAtkBtnClicked.addEventListener('click', event => {
+  //initiate attack sequence...
+  attack();
+})
 
 //triggers when inn image is clicked
 const isInnClicked = document.getElementById("inn");
@@ -133,7 +221,8 @@ const isHuntClicked = document.getElementById("hunt");
 isHuntClicked.addEventListener('click', event => {
   //call random monsters
   getRandomMonster(monsterList);
-  console.log(getRandomMonster);
+  displayMonsterStats(selectedMonster);
+  initBattle();
 })
 
 
